f154f741871fde72c1077737a8b4cde20ed57c03
Omegakxa <fabiorpontel@gmail.com>
2013-12-27 21:51:23 -0300

    Reinserido arquivos para deploy
    Sem estes arquivos o phonegap nao funciona

---
+++ Deploy/17.js
@@ -0,0 +1,6 @@
+
+Form2.innerHTML=[  NSB.Checkbox('CheckBox1', 300, 'Display Date, Auto-clear text, Auto-capitalize,Auto-complete,Auto-correct,Add GPS Coordinates', style='' ),
+  "<label id='Label2' class='graytitle' style='text-align:left;  font-size:16px; font-family:helvetica; font-style:normal; font-weight:bold; color:black; background-color:transparente; border-style:;border-color:transparent;border-width:1px; ' >Export via:</label>",
+  NSB.HeaderBar_jqm("TitleBar2", "Configurações", "Voltar", "arrow-l", "", "false", " data-theme=c style='' class=' ' "),
+  NSB.ButtonBar("ButtonBar1", "true", "Email, Imprimir", "0", " style= "),
+  ].join('');
